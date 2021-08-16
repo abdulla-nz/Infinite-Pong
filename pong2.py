84b57761f77eceeeed6175f4500bcdc4c48c383e
Abdulla Kubba <88988544+abdulla-nz@users.noreply.github.com>
2021-08-16 21:03:20 +1200

    adding Menu
    Added a menu, still requires user information on how to interact.
    
    Need to find way to revert back to menu when game over or lost.

--- pong2.py
+++ pong2.py
@@ -16,6 +16,8 @@ icon = pygame.image.load('icon.png')
 pygame.display.set_icon=(icon)
 
 background = pygame.image.load('background.png') # 1000x500
+menuBackgroundRed = pygame.image.load('menuBackgroundRed.png') # 1000x500
+menuBackgroundWhite = pygame.image.load('menuBackgroundWhite.png') # 1000x500
 
 player = bar("p")
 enemy = bar("e")
@@ -23,37 +25,64 @@ ball = ball()
 # game loop
 
 run = True
+menu = True
+gameRunning = False
+
 while run:
-	win.fill((0,0,0))
 	win.blit(background, (0,0))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
 
-	keys = pygame.key.get_pressed()
+	while menu:
+		if pygame.Rect(200, 150, 600, 150).collidepoint(pygame.mouse.get_pos()) :
+			win.blit(menuBackgroundRed, (0,0))
+		else : win.blit(menuBackgroundWhite, (0,0))
 
-	if keys[pygame.K_UP]:
-		if player.getCoord('y')>0:
-			player.setCoord('y',-1)
-	elif keys[pygame.K_DOWN]:
-		if player.getCoord('y')<400:
-			player.setCoord('y',1)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				menu = False
+				run = False
+			elif event.type == pygame.MOUSEBUTTONDOWN and pygame.Rect(200, 150, 600, 150).collidepoint(pygame.mouse.get_pos()):
+				menu = False
+				gameRunning = True
 
-	if (ball.getCoord('x')>925) :
-		ball.changeDirection()
+		pygame.display.update()
 
-	if (ball.getCoord('x')<65 and (ball.getCoord('y')>player.getCoord('y') and ball.getCoord('y')<player.getCoord('y')+100)) :
-		ball.changeDirection()
+	win.blit(background, (0,0))
+
+	while gameRunning:
+
+		win.blit(background, (0,0))
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameRunning = False
+				run = False
+
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_UP]:
+			if player.getCoord('y')>0:
+				player.setCoord('y',-1)
+		elif keys[pygame.K_DOWN]:
+			if player.getCoord('y')<400:
+				player.setCoord('y',1)
+
+		if (ball.getCoord('x')>925) :
+			ball.changeDirection()
+
+		if (ball.getCoord('x')<65 and (ball.getCoord('y')>player.getCoord('y') and ball.getCoord('y')<player.getCoord('y')+100)) :
+			ball.changeDirection()
 
 
 
-	ball.setCoord('x',ball.getCoord('v'))
+		ball.setCoord('x',ball.getCoord('v'))
 
-	win.blit(player.getImage(), (player.getCoord('x'),player.getCoord('y')))
-	win.blit(enemy.getImage(), (enemy.getCoord('x'),enemy.getCoord('y')))
-	win.blit(ball.getImage(), (ball.getCoord('x'),ball.getCoord('y')))
-	pygame.display.update()
-	pygame.time.delay(3)
+		win.blit(player.getImage(), (player.getCoord('x'),player.getCoord('y')))
+		win.blit(enemy.getImage(), (enemy.getCoord('x'),enemy.getCoord('y')))
+		win.blit(ball.getImage(), (ball.getCoord('x'),ball.getCoord('y')))
+		pygame.display.update()
+		pygame.time.delay(3)
 
 pygame.quit()
 quit()
